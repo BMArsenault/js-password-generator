46db7cdd82892bd2b4dbcbdcfa23f6746cf65f62
Brian <BMArsenault@gmail.com>
2021-10-21 23:15:57 -0700

    add logic for password length

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Array of special characters to be included in password
+// var specialCharacters = @%+\\/'!#$^?:,)(}{][~-_.
+
+// var numericCharacters = 0123456789
+
+// var lowerCasedCharacters = abcdefghijklmnopqrstuvwxyz
+
+// var upperCasedCharacters = ABCDEFGHIJKLMNOPQRSTUVWXYZ
+
+//generate password
+function generatePassword() {
+  const totalLength = prompt("How many characters would you like to use?");
+  if(totalLength >= 8 && totalLength <= 128) {
+    alert('Your new password will be ' + totalLength + ' characters long.')
+  }
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
